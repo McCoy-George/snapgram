602495353e9e8a251e7421e0bcdb9e9659e6ef9f
YouIrfan <img000418@gmail.com>
2023-12-07 23:57:47 +0800

    first commit

--- src/_auth/forms/SigninForm.tsx
+++ src/_auth/forms/SigninForm.tsx
@@ -54,7 +54,8 @@ const SigninForm = () => {
   return (
     <Form {...form}>
       <div className="sm:w-420 flex-center flex-col">
-        <img src="/assets/images/croped.png" alt="logo"className="w-60 h-30" />
+        <img src="/assets/images/croped.png" alt="logo" width={300}
+            height={36}/>
 
         <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">
           Log in to your account
--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -75,7 +75,8 @@ const SignupForm = () => {
   return (
     <Form {...form}>
       <div className="sm:w-420 flex-center flex-col">
-        <img src="/assets/images/croped.png" alt="logo" />
+        <img src="/assets/images/croped.png" alt="logo" width={300}
+            height={36}/>
 
         <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">
           Create a new account
