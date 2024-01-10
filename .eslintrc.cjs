5f24e0d4e01ec5242e807abee7e32ce68cd4eae6
YouIrfan <img000418@gmail.com>
2024-01-11 01:06:52 +0800

    first commit

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,9 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime', 
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
@@ -15,4 +17,4 @@ module.exports = {
       { allowConstantExport: true },
     ],
   },
-}
+};
